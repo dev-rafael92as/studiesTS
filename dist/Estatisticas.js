af0864dd6d42a7b835cd5108ef1cf83e91f32aa0
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2024-01-31 15:48:38 -0300

    [TS-Curso] Exercicio final1 - Included countBy + setPagamento e setStatus

--- dist/Estatisticas.js
+++ dist/Estatisticas.js
@@ -1,15 +1,28 @@
+import countBy from "./countBy.js";
 function filtrarValor(transacao) {
     return transacao.valor !== null;
 }
 export default class Estatisticas {
     transacoes;
     total;
+    pagamento;
+    status;
     constructor(transacoes) {
         this.transacoes = transacoes;
         this.total = this.setTotal();
+        this.pagamento = this.setPagamento();
+        this.status = this.setStatus();
     }
     setTotal() {
         return this.transacoes.filter(filtrarValor).reduce((acc, item) => { return acc + item.valor; }, 0);
+    }
+    setPagamento() {
+        const pagamentos = this.transacoes.map(({ pagamento }) => pagamento);
+        return countBy(pagamentos);
+    }
+    setStatus() {
+        const allStatus = this.transacoes.map(({ status }) => status);
+        return countBy(allStatus);
     }
 }
 //# sourceMappingURL=Estatisticas.js.map
--- src/Estatisticas.ts
+++ src/Estatisticas.ts
@@ -1,3 +1,5 @@
+import countBy from "./countBy.js";
+
 type TransacaoValor = Transacao & { valor: number }
 function filtrarValor(transacao: Transacao): transacao is TransacaoValor {
     return transacao.valor !== null
@@ -6,12 +8,27 @@ function filtrarValor(transacao: Transacao): transacao is TransacaoValor {
 export default class Estatisticas {
     private transacoes;
     total;
+    pagamento;
+    status;
+
     constructor(transacoes: Transacao[]) {
         this.transacoes = transacoes
         this.total = this.setTotal()
+        this.pagamento = this.setPagamento()
+        this.status = this.setStatus()
     }
 
     private setTotal() {
         return this.transacoes.filter(filtrarValor).reduce((acc, item) => {return acc + item.valor}, 0)
+    }
+
+    private setPagamento() {
+        const pagamentos = this.transacoes.map(({pagamento}) => pagamento)
+        return countBy(pagamentos)
+    }
+
+    private setStatus() {
+        const allStatus = this.transacoes.map(({status}) => status)
+        return countBy(allStatus)
     }
 } 
